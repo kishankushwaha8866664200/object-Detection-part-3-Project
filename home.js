aaf135b2c8526bf0e69fec7ef0bd7e151796d648
kishankushwaha8866664200 <84864817+kishankushwaha8866664200@users.noreply.github.com>
2023-04-29 18:35:59 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,22 +1,22 @@
 function redirect(l) { 
 console.log(l);
 if(l == 'Trophy') {
-    window.location = "object1/object.html";
+    window.location = "object1.html";
 }else if(l == "Laptop") {
 
-    window.location = "object2/object.html";
+    window.location = "object2.html";
 
 }else if(l == "Phone") {
 
-    window.location = "object3/object.html";
+    window.location = "object3.html";
 
 }else if(l == "Books") {
 
-    window.location = "object4/object.html";
+    window.location = "object4.html";
 
 }else if(l == "Bottles") {
 
-    window.location = "object5/object.html";
+    window.location = "object5.html";
 
 }
 }
