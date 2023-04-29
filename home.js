fd3d3387458a11f18e43026dee512d59d288aba4
kishankushwaha8866664200 <84864817+kishankushwaha8866664200@users.noreply.github.com>
2023-04-29 18:35:06 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,22 @@
+function redirect(l) { 
+console.log(l);
+if(l == 'Trophy') {
+    window.location = "object1/object.html";
+}else if(l == "Laptop") {
+
+    window.location = "object2/object.html";
+
+}else if(l == "Phone") {
+
+    window.location = "object3/object.html";
+
+}else if(l == "Books") {
+
+    window.location = "object4/object.html";
+
+}else if(l == "Bottles") {
+
+    window.location = "object5/object.html";
+
+}
+}
